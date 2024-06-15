786308d9454a458f166825de0b9e258cb0dab6d9
zheer barzan <zheerbarzan85@outlook.com>
2024-06-15 18:14:21 +0300

    adding the themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_music/home_page.dart';
+import 'package:project_music/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
