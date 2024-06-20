d87fed0d0796f22af023c851ae280ca8e504e5fb
Zheer Barzan <zheerbarzan85@outlook.com>
2024-06-20 14:42:57 +0300

    making the home and settings

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -10,15 +10,38 @@ class MyDrawer extends StatelessWidget {
         child: Column(
           children: [
             // logo
-            DrawerHeader(
+            const DrawerHeader(
               child: Center(
                   child: Icon(
                 Icons.music_note,
               )),
-            )
+            ),
             //home tile
-
+            ListTile(
+              title: const Row(
+                children: [
+                  Text('H O M E'),
+                  Spacer(),
+                  Icon(Icons.home),
+                ],
+              ),
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              },
+            ),
             // setting tile
+            ListTile(
+              title: const Row(
+                children: [
+                  Text('S E T T I N G S'),
+                  Spacer(),
+                  Icon(Icons.settings),
+                ],
+              ),
+              onTap: () {
+                Navigator.pushNamed(context, '/settings');
+              },
+            ),
 
             //
           ],
