f8029a83833b47e1d575d33981d3d9d2931d3755
Zheer Barzan <zheerbarzan85@outlook.com>
2024-06-23 15:22:48 +0300

    making a playlist

--- lib/models/song.dart
+++ lib/models/song.dart
@@ -1,1 +1,8 @@
-class Song {}
+class Song {
+  final String songName;
+  final String artistName;
+  final String albumArtImagePath;
+  final String songPath;
+
+  Song(this.songName, this.artistName, this.albumArtImagePath, this.songPath);
+}
