736e1999e145ff2dc850e838fc27fd6cb5b13fff
Zheer Barzan <zheerbarzan85@outlook.com>
2024-06-24 19:40:36 +0300

    adding other songs

--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -10,5 +10,23 @@ class PlaylistProvider extends ChangeNotifier {
       albumArtImagePath: "assets/images/am.jpg",
       songPath: "assets/audio/505.mp3",
     ),
+    Song(
+      songName: "Do I Wanna Know?",
+      artistName: "Arctic Monkeys",
+      albumArtImagePath: "assets/images/am.jpg",
+      songPath: "assets/audio/Do I Wanna Know.mp3",
+    ),
+    Song(
+      songName: "I Wanna Be Yours",
+      artistName: "Arctic Monkeys",
+      albumArtImagePath: "assets/images/am.jpg",
+      songPath: "assets/audio/I Wanna Be Yours.mp3",
+    ),
+    Song(
+      songName: "R U Mine",
+      artistName: "Arctic Monkeys",
+      albumArtImagePath: "assets/images/am.jpg",
+      songPath: "assets/audio/R U Mine.mp3",
+    ),
   ];
 }
