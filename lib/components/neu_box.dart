82d5e1887b468df8bc278b22f0862c7891f8345f
zheer barzan <zheerbarzan85@outlook.com>
2024-06-26 17:43:57 +0300

    making the song page neumorphic

---
+++ lib/components/neu_box.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class NeuBox extends StatelessWidget {
+  final Widget? child;
+  const NeuBox({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.background,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade500,
+              blurRadius: 15,
+              offset: const Offset(5, 5),
+            ),
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 15,
+              offset: const Offset(-5, -5),
+            ),
+          ]),
+      padding: const EdgeInsets.all(8),
+      child: child,
+    );
+  }
+}
--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:project_music/components/neu_box.dart';
 
 class SongPage extends StatelessWidget {
   const SongPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        title: const Text('Song Page'),
+      ),
+      body: const Center(
+        child: NeuBox(child: Icon(Icons.abc)),
+      ),
+    );
   }
 }
