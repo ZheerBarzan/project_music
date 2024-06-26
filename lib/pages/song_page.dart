049a9d45b5ca5589ff11ed8c43e87bd196236e4e
zheer barzan <zheerbarzan85@outlook.com>
2024-06-26 17:58:44 +0300

    image problem agian

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:project_music/components/neu_box.dart';
+import 'package:project_music/models/playlist_provider.dart';
+import 'package:provider/provider.dart';
 
 class SongPage extends StatelessWidget {
   const SongPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        title: const Text('Song Page'),
-      ),
-      body: const Center(
-        child: NeuBox(child: Icon(Icons.abc)),
+    return Consumer<PlaylistProvider>(
+      builder: (context, value, child) => Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: SafeArea(
+          child: Column(
+            children: [
+              // app bar
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // back button
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+
+                  // title
+
+                  Text("P L A Y L I S T"),
+
+                  // menu button
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.menu),
+                  ),
+                ],
+              ),
+
+              // album artwork
+
+              NeuBox(child: Image.asset("lib/assets/images/am.jpg")),
+
+              // song duration process
+
+              // controls
+            ],
+          ),
+        ),
       ),
     );
   }
