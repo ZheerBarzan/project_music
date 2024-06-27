5ae93c25b5143bff01bfbd93fa1f84a430e2bcd0
Zheer Barzan <zheerbarzan85@outlook.com>
2024-06-27 18:59:42 +0300

    making the UI a bit nicer

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -12,42 +12,69 @@ class SongPage extends StatelessWidget {
       builder: (context, value, child) => Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
         body: SafeArea(
-          child: Column(
-            children: [
-              // app bar
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // back button
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.arrow_back),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                // app bar
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // back button
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+
+                    // title
+
+                    Text("P L A Y L I S T"),
+
+                    // menu button
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.menu),
+                    ),
+                  ],
+                ),
+
+                // album artwork
+
+                NeuBox(
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset("assets/images/am.jpg"),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Column(
+                              children: [
+                                Text("505"),
+                                Text("Arctic Monkeys"),
+                              ],
+                            ),
+
+                            //heart Icon
+                          ],
+                        ),
+                      )
+                    ],
                   ),
+                ),
 
-                  // title
+                // song duration process
 
-                  Text("P L A Y L I S T"),
-
-                  // menu button
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.menu),
-                  ),
-                ],
-              ),
-
-              // album artwork
-
-              NeuBox(child: Image.asset("assets/images/am.jpg")),
-
-              // song duration process
-
-              // controls
-            ],
+                // controls
+              ],
+            ),
           ),
         ),
       ),
